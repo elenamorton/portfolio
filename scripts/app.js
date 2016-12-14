720ad03fc0d0f3d9b10bcb0b37f3127ff630b0b5
emorton <elena.uleia@gmail.com>
2016-12-14 15:42:54 +0000

    Added background image based on weather conditions

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-var main = function() {
+function main() {
   var $menu = $('#sidebar-wrapper');
   $('#close-btn').click(function() {
     $menu.toggleClass('open');
@@ -16,7 +16,62 @@ var main = function() {
     }
   });
   
-  return false;
+  getWeather();
+
+  function getWeather() {
+    var longitude = -0.090000;
+    var latitude = 51.520000;
+    $.ajax({
+      type: 'GET',
+      url: 'http://api.wunderground.com/api/291d6d87200cc037/geolookup/conditions/q/' + latitude + ',' + longitude + '.json',
+      dataType: 'json',
+      success: function(response) {
+        var conditions = response.current_observation.weather;
+        var location = response.location.tz_long;
+
+        loadImage(conditions);
+      }
+    });
+  }
+
+  function getTimeOfDay() {
+    var time = new Date();
+    var hours = time.getHours();
+    var timeOfDay;
+
+    if (hours > 17) {
+      timeOfDay = 'night';
+    } else if (hours > 12) {
+      timeOfDay = 'afternoon';
+    } else {
+      timeOfDay = 'morning';
+    }
+
+    return timeOfDay;
+  }
+
+  function loadImage(conditions) {
+    var imageSRC = 'img/weather/hero-';
+    var timeofDay = getTimeOfDay();
+    var validConditions = ['clear', 'mostly cloudy', 'overcast', 'rain', 'snow'];
+    var conditions = conditions.toLowerCase();
+
+    if (validConditions.indexOf(conditions) === -1) {
+      conditions = 'clear';
+    }
+
+    //to add conditions.join();
+    if (conditions === 'mostly cloudy') {
+      conditions = 'mostlycloudy'
+    }
+
+    imageSRC = imageSRC + conditions + '-' + timeofDay + '.jpg';
+
+    $('#intro').css('background-image', 'url(' + imageSRC + ')')
+  }
+  
+  
+ // return false;
 };
 
 $(document).ready(main);
